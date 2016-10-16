824a6cbb9ecf7cd95fa5aeca74f3936338dff295
Pedro Fraga <antoniopedrofraga@gmail.com>
2016-10-16 02:28:55 +0100

    android project added

---
+++ code/android-workshop-codeweek/app/src/main/java/me/pedrofraga/workshop/Menu.java
@@ -0,0 +1,13 @@
+package me.pedrofraga.workshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_layout);
+    }
+}
